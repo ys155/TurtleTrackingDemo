9c0cc5701eebd8bf12fabca7dc9b8e363ef6c8ac
ys155 <ys155@duke.edu>
2016-10-03 22:26:07 -0400

    modified script name

--- TurtleTracking.py
+++ TurtleTracking.py
@@ -1,9 +1,9 @@
-# Task1.py
+# TurtleTracker.py
 #
 # Description: Parses a line of ARGOS tracking data 
 #
 # Created by: John Fay (jpfay@duke.edu)
-# Created on: Oct 2012
+# Created on: Oct 2016
 
 # Copy and paste a line of data as the startLine variable value
 lineString = "20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-126	529	3	401 651134.7	0"
